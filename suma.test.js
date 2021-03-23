99c5e4976960f9aa0a704732a71a1ff55e39de90
Boukern <boukern@gmail.com>
2021-03-23 05:25:36 +0100

    suma CI version1

--- suma.test.js
+++ suma.test.js
@@ -1,7 +1,19 @@
 const suma = require('./suma');
 
 test('2 + 5 = 7', () =>{
+    expect(suma(2,5)).toBe(8);
+});
+
+test('2 + 5 = 7', () =>{
     expect(suma(2,5)).toBe(7);
+});
+
+test('9 + 1 = 10', () =>{
+    expect(suma(9,1)).toBe(11);
+});
+
+test('8 + 4 = 12', () =>{
+    expect(suma(8,4)).toBe(12);
 });
 
 test('a + 5 = NaN', () =>{
